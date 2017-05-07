ecd3975e481daaaf79f583bf7f77365b649c8b89
Andre Stackhouse <CaptainStack@outlook.com>
2017-05-06 21:59:43 -0700

    Minor fixes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,10 +28,10 @@ export const App = ({state}) => {
         <Game tiles={state.board} game_in_progress={!state.hasWon()} />
         <p><strong>HOW TO PLAY:</strong> Left-click to advance tiles to the next color. Right-click to change them to their previous. There are 6 colors in total that appear in the order red, orange, yellow, green, blue, white.</p>
         <hr />
-        <p>Created by <a href='https://captainstack.github.io/public-stackhouse' target='_'>Andre Stackhouse.</a> Source code <a href='https://github.com/CaptainStack/chromattis' target='_'>hosted on GitHub</a> under an open source MIT license.</p>
+        <p>Created by <a href='https://captainstack.github.io/public-stackhouse' target='_'><strong>Andre Stackhouse</strong></a>. Source code hosted on <a href='https://github.com/CaptainStack/chromattis' target='_'><strong>GitHub</strong></a> under an open source MIT license.</p>
         {/* SOCIAL MEDIA BUTTONS */}
         <div className='row'>
-          <a href="https://twitter.com/share" className="twitter-share-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charSet="utf-8"></script>
+          <a href="https://twitter.com/share?text=Are%20you%20smart%20enough%20to%20solve%20#Chromattis%3F&via=CaptainStack" className="twitter-share-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charSet="utf-8"></script>
           <div className="fb-share-button" data-href="https://captainstack.github.io/chromattis/" data-layout="button_count" data-size="small" data-mobile-iframe="false"><a className="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcaptainstack.github.io%2Fchromattis%2F&amp;src=sdkpreparse">Share</a></div>
           <a href="//www.reddit.com/submit" onClick={() => { window.location = '//www.reddit.com/submit?url=' + encodeURIComponent(window.location); return false }}> <img src="//www.redditstatic.com/spreddit7.gif" alt="submit to reddit" /> </a>
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
