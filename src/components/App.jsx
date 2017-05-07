5e03e0040406cf2320a3caad265b19a7c53f087f
Andre Stackhouse <CaptainStack@outlook.com>
2017-05-06 22:35:45 -0700

    Update support button.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,7 @@ export const App = ({state}) => {
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type="hidden" name="cmd" value="_s-xclick" />
             <input type="hidden" name="hosted_button_id" value="TFH72M6LQSGNG" />
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+            <input type="submit" className='flat-button' value="Support via PayPal" alt="PayPal - The safer, easier way to pay online!" />
             <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" />
           </form>
         </div>
