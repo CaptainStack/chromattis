ed6792c64162a82cd914cbde814049aee9de765a
Andre Stackhouse <CaptainStack@outlook.com>
2017-11-21 02:28:09 -0800

    Move gridbox styles inline for reactive layout.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,7 +5,13 @@ import '../styles/Game.css';
 export const Game = ({tiles, game_in_progress, current_moves, current_level_index}) => {
   tiles = tiles.map(tile => <Tile key={tile.id} tile={tile} game_in_progress={ game_in_progress }/>);
   return (
-    <div className='Game' onContextMenu={event => event.preventDefault() } style={{ display: game_in_progress ? null : 'none' }} >
+    <div className='Game' 
+         onContextMenu={event => event.preventDefault() } 
+         style={{ 
+           display: game_in_progress ? null : 'none',
+           gridTemplateColumns: `repeat(${ Math.sqrt(tiles.length) }, ${ 600 / Math.sqrt(tiles.length) - 15 }px)`,
+           gridTemplateRows: `repeat(${ Math.sqrt(tiles.length) }, ${ 600 / Math.sqrt(tiles.length) - 15 }px)`,
+           gridGap: '15px' }} >
       { tiles }
     </div>
   );
