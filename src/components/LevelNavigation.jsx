1ceb6841f66dc0602aad91c4550801942add8696
Andre Stackhouse <CaptainStack@outlook.com>
2017-12-03 18:16:24 -0800

    Create LevelNavigation component.

---
+++ src/components/LevelNavigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/LevelNavigation.css';
+import { navigateLevelButtonClicked } from '../events';
+
+export const LevelNavigation = ({levels, current_level_index, highest_unlocked_level}) => {
+
+  let buttons = levels.map((level, index) => {
+
+    let current_level_class = current_level_index === index ? 'current-level' : null;
+    let is_unlocked = highest_unlocked_level >= index;
+    let locked_class = is_unlocked ? 'unlocked' : 'locked';
+
+    return (
+      <a key={ index } 
+         className={`level-navigation-button ${ current_level_class } ${ locked_class }`} 
+         onClick={ is_unlocked ? navigateLevelButtonClicked(index) : null }
+         title={ is_unlocked ? `Click to go to level ${index}` : 'Locked' } >
+         {index}
+      </a>
+    )
+  })
+
+  return(<div className='LevelNavigation'>Select Level: {buttons}</div>);
+}
+export default LevelNavigation;
