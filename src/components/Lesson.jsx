98f1cb507e8c95e7106c5212f6ef79ee11b61670
Andre Stackhouse <captainstack@gmail.com>
2024-11-13 22:18:15 -0800

    Move lesson text to bottom for easier readability

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -3,8 +3,8 @@ const ASSET_PATH = window.location.host.includes('localhost') ? './chromattis/tu
 export const Lesson = ({lesson}) => {
   return(
     <div className='Lesson'>
-      <span>{lesson.text}</span>
       <img src={`${ASSET_PATH}${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/>
+      <span>{lesson.text}</span>
     </div>
   )
 }
