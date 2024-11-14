58174cbfa214bd45d12cefea39e532c644540b88
Andre Stackhouse <captainstack@gmail.com>
2024-11-13 21:53:57 -0800

    Add canonical asset path

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,8 +1,10 @@
+const ASSET_PATH = window.location.host.includes('localhost') ? './chromattis/tutorial/' : './tutorial/';
+
 export const Lesson = ({lesson}) => {
   return(
     <div className='Lesson'>
       <span>{lesson.text}</span>
-      <img src={`./chromattis/tutorial/${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/>
+      <img src={`${ASSET_PATH}${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/>
     </div>
   )
 }
