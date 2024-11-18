12d35e7395a3c72c9ac89504ab89ecd19bdc3b7d
Andre Stackhouse <captainstack@gmail.com>
2024-11-17 17:34:57 -0800

    Remove unnecessary syntax

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,4 +1,4 @@
-export const Lesson = ({lesson}) => {
+export const Lesson = lesson => {
   return(
     <div className='Lesson'>
       <img src={`${process.env.PUBLIC_URL}/${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/><br/>
