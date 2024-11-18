66ded69fbbad6d8c08f6b3296c5ad255da60636e
Andre Stackhouse <captainstack@gmail.com>
2024-11-17 17:32:11 -0800

    Check in built in PUBLIC_URL constant

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,9 +1,7 @@
-const ASSET_PATH = window.location.host.includes('localhost') ? './chromattis/tutorial/' : './tutorial/';
-
 export const Lesson = ({lesson}) => {
   return(
     <div className='Lesson'>
-      <img src={`${ASSET_PATH}${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/><br/>
+      <img src={`${process.env.PUBLIC_URL}/${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/><br/>
       <span>{lesson.text}</span>
     </div>
   )
