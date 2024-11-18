76fdaa9e44667751e7a45fbcb7f7c1ad6fc2eec4
Andre Stackhouse <captainstack@gmail.com>
2024-11-17 17:56:17 -0800

    Fix broken URL

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,7 +1,7 @@
 export const Lesson = lesson => {
   return(
     <div className='Lesson'>
-      <img src={`${process.env.PUBLIC_URL}/${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/><br/>
+      <img src={`${process.env.PUBLIC_URL}/tutorial/${lesson.image}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/><br/>
       <span>{lesson.text}</span>
     </div>
   )
