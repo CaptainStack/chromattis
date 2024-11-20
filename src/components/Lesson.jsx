a059a831d7bf6573401d854a2e3fce33d2d32b2d
Andre Stackhouse <captainstack@gmail.com>
2024-11-19 17:45:17 -0800

    Compress data URLs, move import from Lesson to Tutorial

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,9 +1,7 @@
-import { IMAGE_URLS } from "../image_urls";
-
-export const Lesson = ({lesson}) => {
+export const Lesson = ({lesson, image_url}) => {
   return(
     <div className='Lesson'>
-      <img src={IMAGE_URLS[lesson.image]} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/><br/>
+      <img src={`data:image/gif;base64,${image_url}`} style={{width:'400px', height:'400px', marginBottom:'10px', borderRadius:'10px', border: '10px solid transparent'}}/><br/>
       <span>{lesson.text}</span>
     </div>
   )
