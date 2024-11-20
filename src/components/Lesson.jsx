3c34cbd568e3f989a3231ecd3f77310f8a622d85
Andre Stackhouse <captainstack@gmail.com>
2024-11-20 05:06:39 -0800

    Spacing tweaks

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -3,7 +3,7 @@
 export const Lesson = ({lesson, current_level}) => {
   return(
     <div className='Lesson'>
-      <img src={`${process.env.PUBLIC_URL}/tutorials/${lesson.image}`} style={{width:'390px', height:'390px', margin:0, borderRadius:'10px'}}/><br/>
+      <img src={`${process.env.PUBLIC_URL}/tutorials/${lesson.image}`} style={{width:'390px', height:'390px', marginBottom:'8px', borderRadius:'10px'}}/><br/>
       <strong>LESSON {current_level}: {lesson.text}</strong>
     </div>
   )
