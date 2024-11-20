40420870f212d0d13d70107f73faa20013c919dd
Andre Stackhouse <captainstack@gmail.com>
2024-11-20 13:02:47 -0800

    Fix LevelNavMenu locking param

--- src/components/LevelNavMenu.jsx
+++ src/components/LevelNavMenu.jsx
@@ -12,9 +12,8 @@ export const LevelNavMenu = ({levels, current_level_index, highest_unlocked_leve
   let buttons = levels.map((level, index) => {
     let current_level_class = current_level_index === index ? 'current-level' : null;
     let is_unlocked = highest_unlocked_level >= index;
-    let locked_class = is_unlocked ? 'unlocked' : 'locked';
 
-    return <LevelNavButton display_in_nav_menu={index >= lower_index && index < upper_index} show_level_nav={true} key={`${level.id}-NavButton`} level={level} index={index} current_level_index={current_level_index} locked_class={locked_class} current_level_class={current_level_class}/>;
+    return <LevelNavButton display_in_nav_menu={index >= lower_index && index < upper_index} show_level_nav={true} key={`${level.id}-NavButton`} level={level} index={index} current_level_index={current_level_index} is_unlocked={is_unlocked} current_level_class={current_level_class}/>;
   })
 
   return(
