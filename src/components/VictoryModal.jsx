a14ab56f2cb8918afeebe740fd65cfdbed62cd85
Andre Stackhouse <captainstack@gmail.com>
2024-11-22 07:37:29 -0800

    Update Reddit share post text

--- src/components/VictoryModal.jsx
+++ src/components/VictoryModal.jsx
@@ -28,7 +28,7 @@ export const VictoryModal = ({show_victory, game_in_progress, current_moves, cur
     <div className='row victory-share' style={{ display: game_in_progress ? 'none' : null }}>
       <a className='social-share' href={`//twitter.com/share?text=${encodeURIComponent(`I just beat #Chromattis Level ${current_level_index} in ${current_moves} ${current_moves === 1 ? 'move' : 'moves'}. I bet you can't do it in fewer!\n\nPlay for free here:\n`)}`} target='_blank'><img src={`${process.env.PUBLIC_URL}/x.png`} alt='Post on X'/></a>
       <a className='social-share' href="//www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcaptainstack.github.io%2Fchromattis%2F&amp;src=sdkpreparse" target="_blank" rel="noopener noreferrer" ><img src={`${process.env.PUBLIC_URL}/facebook.png`} alt='Share on Facebook' target="_blank" rel="noopener noreferrer" /></a>
-      <a className='social-share' href={`//www.reddit.com/submit?url=${encodeURIComponent(window.location)}&title=${encodeURIComponent('Just discovered a new puzzle game called Chromattis. It\'s fun but very challenging (and free)!')}`} target='_blank'> <img src={`${process.env.PUBLIC_URL}/reddit.png`} alt="Submit to Reddit"/></a>
+      <a className='social-share' href={`//www.reddit.com/submit?url=${encodeURIComponent(window.location)}&title=${encodeURIComponent('Just discovered a new puzzle game called Chromattis. It\'s challenging but very fun (and free)!')}`} target='_blank'> <img src={`${process.env.PUBLIC_URL}/reddit.png`} alt="submit to reddit"/></a>
     </div>
   </div>
 
