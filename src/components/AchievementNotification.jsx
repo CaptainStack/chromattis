37835d4243215d098def57ee6e2ec5311d113bf2
Andre Stackhouse <captainstack@gmail.com>
2025-05-12 04:38:08 -0700

    Tweak AchievementNotification display styles

--- src/components/AchievementNotification.jsx
+++ src/components/AchievementNotification.jsx
@@ -3,7 +3,7 @@ import { achievementsButtonClicked } from '../events';
 
 export const AchievementNotification = ({state}) => {
   return(
-    <div id='AchievementNotification' className='hide' style={{position:'absolute', top:'0', padding:'16px 0 16px 0', backgroundColor:'goldenrod', borderRadius:'10px', width:'100%', zIndex:'10', cursor:'pointer'}} onClick={achievementsButtonClicked}>
+    <div id='AchievementNotification' className='hide' onClick={achievementsButtonClicked}>
       <strong>Achievement Unlocked</strong>
       <p>{state.achievement_text}</p>
     </div>
